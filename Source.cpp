b1e109c34ec8b1cb4f3b479751e185daf87e01b3
Alexander Recalde <apache@vps158787.vps.ovh.ca>
2017-12-03 00:41:32 -0500

    README Update

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,10 @@
+/*
+Final project for CIS 250 Intro to Object Oriented Programming
+Created by Alexander Recalde
+https://github.com/arecalde/Sudoku
+*/
 #include "Soduko.h"
 using namespace std;
-
 int main() {
 	Sudoku game1;
 	game1.beginNewGame();
